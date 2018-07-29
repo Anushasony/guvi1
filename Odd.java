4c16b66591eec0068c31157e87cbe874989b9cd9
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-07-29 10:39:00 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -1,17 +1,24 @@
-import java.io.*;
+/* package whatever; // don't place package name! */
+
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Odd
 {
-  public static void main(String[] args) throws java.lang.Exception
-  {
-      Scanner s=new Scanner(System.in);
-      int x=s.nextInt();
-      int y=s.nextInt();
-      for(int i=x+1;i<y;i++)
-      {
-        if(i%2 !=0)
-              System.out.print(i+" ");
-      }
-  
-  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		for(int i=a+1;i<=b-1;i++)
+		   {
+		   	if(i%2!=0)
+		   	    {
+		   	    	System.out.println(i+" ");
+		   	    }
+		   }
+	}
 }
