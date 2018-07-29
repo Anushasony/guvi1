00c9513c0f0a5ddef244171404f9bc932cc69975
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-07-29 10:49:00 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -13,7 +13,7 @@ class Odd
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-		for(int i=a+1;i<=b-1;i++)
+		for(int i=a+1;i<b;i++)
 		   {
 		   	if(i%2!=0)
 		   	    {
