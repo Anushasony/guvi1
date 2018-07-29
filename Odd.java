8052bd5e08fd6d1b37ac00f9a2b3ac4ab0f6337e
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-07-29 10:02:09 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Odd
+{
+  public static void main(String[] args)
+  {
+      Scanner s=new Scanner(System.in);
+      int x=s.nextInt();
+      int y=s.nextInt();
+      for(int i=x+1;i<y;i++)
+      {
+        if(i%2 !=0)
+              System.out.println(i);
+      }
+  
+  }
+}
