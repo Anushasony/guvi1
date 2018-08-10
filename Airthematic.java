81cda4bd578723112ebb74d7233537807197f73f
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-08-10 21:26:43 +0530

    Create Airthematic.java

---
+++ Airthematic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Airthematic
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n,a,d;
+		n=s.nextInt();
+		a=s.nextInt();
+		d=s.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+a;
+			a=a+d;
+    }
+	  System.out.println(sum);
+	}
+}
