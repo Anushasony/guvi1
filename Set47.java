bc5cd08075c917427ae3f3ecf98b1e1824fd16c1
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-08-11 13:50:03 +0530

    Create Set47.java

---
+++ Set47.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Set47
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int t;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println(a+" "+b);
+	}
+}
