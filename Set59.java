9e14905a92186d2a1827b94ddfe86f3fd1c11a1a
Anushasony <41734877+Anushasony@users.noreply.github.com>
2018-08-12 12:01:42 +0530

    Create Set59.java

---
+++ Set59.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Set59
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int p=s.nextInt();
+		System.out.print(n+p);
+	}
+}
